22799558125e5813074611654f0cfa1444a53ac9
Venkat Arun <venkatarun95@gmail.com>
2021-08-18 22:31:35 -0400

    Working version of web page

--- ccac_model.js
+++ ccac_model.js
@@ -8,7 +8,7 @@ var cur_index = null;
 
 function svg_elem(svg, name, args, content=null) {
     let e = document.createElementNS(svgns, name);
-    for (k in args) {
+    for (let k in args) {
 	e.setAttributeNS(null, k, args[k]);
     }
     if (content != null) {
@@ -32,7 +32,7 @@ function click_location(evt, graph, line) {
 
     // Find out which square (if any) the user clicked
     var index = null;
-    for (i in line.pts) {
+    for (let i in line.pts) {
     	if (Math.abs(graph.x_coord(line.pts[i][0]) - gpt.x) <= graph.handle_side &&
             Math.abs(graph.y_coord(line.pts[i][1]) - gpt.y) <= graph.handle_side) {
 	    index = i;
@@ -44,66 +44,69 @@ function click_location(evt, graph, line) {
 }
 
 
-function onLineMouseDown(evt, graph, line) {
+function onLineMouseDown(evt, graph, line_name) {
+    const line = lines[line_name];
     const vals = click_location(evt, graph, line);
     const x = vals[0]; const y = vals[1]; const index = vals[2];
 
     if (index != null) {
 	drag = true;
-	cur_line = line;
+	cur_line = line.name;
 	cur_index = index;
     }
 }
 
 function onLineDblClick(evt, graph, line) {
-    const vals = click_location(evt, graph, line);
+    const vals = click_location(evt, graph, lines[line]);
     const x = vals[0]; const y = vals[1]; const index = vals[2];
 
-    console.log("Double click");
-
-    const lines_clone = jQuery.extend(true, {}, lines);
+    const lines_clone = _.cloneDeep(lines); // jQuery.extend(true, {}, lines);
     if (index == null) {
 	// Create a new point
-	for (i in line.pts) {
-	    if (i == line.pts.length - 1)
+	for (let i in lines[line].pts) {
+	    if (i == 0)
 		continue;
 	    // Can we insert it here?
-	    if (x > line.pts[i][0]) {
-		line.pts.splice(i+1, 0, [x, line.get_y(x)])
+	    if (x >= lines[line].pts[i-1][0] &&
+		x <= lines[line].pts[i][0]) {
+		lines[line].pts.splice(i, 0, [x, lines[line].get_y(x)]);
+		break;
 	    }
-	    break;
 	}
     }
-    const check = check_ccac_constraints(line.name);
+    else {
+	lines[line].pts.splice(index, 1);
+    }
+
+    const check = check_ccac_constraints(line);
     if (!check[0]) {
 	lines = lines_clone;
 	return;
     }
-    graph.plot_line(line);
-    for (i in check[1]) {
+    graph.plot_line(lines[line]);
+    for (let i in check[1]) {
 	graph.plot_line(lines[check[1][i]]);
     }
 }
 
 function onLineMouseMove(evt, graph) {
-    if (!drag || Date.now() - last_move < 100) {
+    if (!drag || Date.now() - last_move < 10) {
 	return;
     }
     last_move = Date.now();
 
-    const line = cur_line;
     const index = cur_index;
 
-    const vals = click_location(evt, graph, line);
+    const vals = click_location(evt, graph, lines[cur_line]);
     const x = vals[0]; const y = vals[1];
 
     if (index != null) {
 	// let bkp = line.pts[index];
 
-	const lines_clone = jQuery.extend(true, {}, lines);
+	const lines_clone = _.cloneDeep(lines); //jQuery.extend(true, {}, lines);
 
-	line.pts[index] = [x, y];
-	const check = check_ccac_constraints(line.name);
+	lines[cur_line].pts[index] = [x, y];
+	const check = check_ccac_constraints(cur_line);
 	// if (!check[0]) {
 	//     line.pts[index][0] = bkp[0];
 	//     line.pts[index][1] = bkp[1];
@@ -117,8 +120,8 @@ function onLineMouseMove(evt, graph) {
 	    return;
 	}
 
-	graph.plot_line(line);
-	for (i in check[1]) {
+	graph.plot_line(lines[cur_line]);
+	for (let i in check[1]) {
 	    graph.plot_line(lines[check[1][i]]);
 	}
     }
@@ -127,6 +130,7 @@ function onLineMouseMove(evt, graph) {
 function onLineMouseUp(evt) {
     drag = false;
     cur_line = null;
+    cur_index = null
 }
 
 // A line in a graph
@@ -140,11 +144,13 @@ class Line {
 
     // Get the y coordinate at given x coordinate (returns null if out of range)
     get_y(x) {
-	for (i in this.pts) {
+	for (let i in this.pts) {
 	    if (i == 0) {
 		continue;
 	    }
-	    console.assert(this.pts[i-1][0] <= this.pts[i][0]);
+	    if (this.pts[i-1][0] > this.pts[i][0]) {
+		console.log("Error: line's x coordinates are not increasing")
+	    }
 	    if (this.pts[i-1][0] <= x && this.pts[i][0] >= x) {
 		return this.pts[i-1][1] + (this.pts[i][1] - this.pts[i-1][1]) * (x - this.pts[i-1][0]) / (this.pts[i][0] - this.pts[i-1][0]);
 	    }
@@ -182,6 +188,47 @@ class Graph {
 	    "y2": this.y1,
 	    "style": "stroke:rgb(0,0,0);stroke-width:2"
 	});
+	svg_elem(this.svg, "text", {
+	    "x": (this.x0 + this.x1) / 2,
+	    "y": (this.y0 + this.height) / 2,
+	    "style": "stroke:rgb(0,0,0);stroke-width:1"
+	}, document.createTextNode("Time (in RTTs)"));
+	svg_elem(this.svg, "text", {
+	    "x": this.x0 / 2,
+	    "y": (this.y0 + this.y1) / 2,
+	    "style": "stroke:rgb(0,0,0);stroke-width:1",
+	    "transform": "rotate(-90 " + this.x0 / 2 + " " + (this.y0 + this.y1) / 2 + ")"
+	}, document.createTextNode("Bytes (in BDP)"));
+
+	for (let t = 0; t <= T; t += 1) {
+	    svg_elem(this.svg, "line", {
+		"x1": this.x_coord(t),
+		"y1": this.y0 - 5,
+		"x2": this.x_coord(t),
+		"y2": this.y0 + 5,
+		"style": "stroke:rgb(0,0,0);stroke-width:2"
+	    });
+	    svg_elem(this.svg, "text", {
+		"x": this.x_coord(t) - 5,
+		"y": this.y0 + 20,
+		"style": "stroke:rgb(0,0,0);stroke-width:1"
+	    }, document.createTextNode(t));
+	}
+	for (let b = 1; b * C < this.yrange[1]; b += 1) {
+	    svg_elem(this.svg, "line", {
+		"x1": this.x0 - 5,
+		"y1": this.y_coord(b),
+		"x2": this.x0 + 5,
+		"y2": this.y_coord(b),
+		"style": "stroke:rgb(0,0,0);stroke-width:2"
+	    });
+	    svg_elem(this.svg, "text", {
+		"x": this.x0 - 15,
+		"y": this.y_coord(b) + 5,
+		"style": "stroke:rgb(0,0,0);stroke-width:1"
+	    }, document.createTextNode(b));
+
+	}
     }
 
     // From line coordinates to SVG coordinates
@@ -212,6 +259,8 @@ class Graph {
 		"draggable": true,
 	    });
 	    l = document.getElementById(this.svg_id + "-" + line.name);
+	    l.addEventListener("mousedown", function(evt) {onLineMouseDown(evt, graph, line.name);});
+	    l.addEventListener("dblclick", function(evt) {onLineDblClick(evt, graph, line.name);});
 	}
 
 	// Make the path command
@@ -236,8 +285,6 @@ class Graph {
 
 	l.setAttributeNS(null, "d", cmd);
 	let graph = this;
-	l.addEventListener("mousedown", function(evt) {onLineMouseDown(evt, graph, line);});
-	l.addEventListener("dblclick", function(evt) {onLineDblClick(evt, graph, line);});
     }
 }
 
@@ -248,7 +295,6 @@ function check_ccac_constraints(line_name) {
     var res = true;
     var changed = [];
     var msgs = "";
-    var L_clone = null; var U_clone = null;
 
 
     // Fix the first and last points' x coordinates. Since we need to do this
@@ -275,9 +321,9 @@ function check_ccac_constraints(line_name) {
 
     // Check monotonicity
     const monotones = ["A", "S", "L", "U"];
-    for (i in monotones) {
+    for (let i in monotones) {
 	const m = monotones[i];
-	for (j in lines[m].pts) {
+	for (let j in lines[m].pts) {
 	    if (j == 0) continue;
 	    if (lines[m].pts[j-1][0] > lines[m].pts[j][0]) {
 		msgs += "The " + m + " curve must be monotonic. ";
@@ -290,38 +336,64 @@ function check_ccac_constraints(line_name) {
 	}
     }
 
-    // S <=  A
-    for (i in lines["S"].pts) {
-	const pt = lines["S"].pts[i];
-	if (pt[1] > lines["A"].get_y(pt[0])) {
-	    msgs += "Cannot serve more packets than have arrived! Hence S <= A must hold. ";
-	    return [false, changed, msgs];
-	}
-    }
-    for (i in lines["A"].pts) {
-	const pt = lines["A"].pts[i];
-	if (pt[1] < lines["S"].get_y(pt[0])) {
-	    msgs += "Cannot serve more packets than have arrived! Hence S <= A must hold. ";
-	    return [false, changed, msgs];
-	}
-    }
-
     // If U or L were modified, change the other one to match
     if (line_name == "U") {
-	L_clone = new Line("L", lines["U"].pts.map(pt => [pt[0] + D, pt[1]]), "black")
+	lines["L"] = new Line("L", lines["U"].pts.map(pt => [pt[0] + D, pt[1]]), "black")
     }
     if (line_name == "L") {
-	U_clone = new Line("U", lines["L"].pts.map(pt => [pt[0] - D, pt[1]]), "black")
+	lines["U"] = new Line("U", lines["L"].pts.map(pt => [pt[0] - D, pt[1]]), "black")
     }
 
-    if (res) {
-	if (U_clone != null) {
-	    lines["U"] = U_clone;
-	    changed.push("U");
+    // Compare the following pairs of things. The first one must be <= the other
+    const cmp = [["S", "A", "Cannot serve more packets than have arrived! Hence S(t) <= A(t) must hold. "],
+		 ["S", "U", "We must have S(t) <= U(t) because we cannot transmit until we have enough tokens. "],
+		 ["L", "S", "We must have L(t) <= S(t) since otherwise the tokens will expire. "]];
+    for (let j in cmp) {
+	const lo = cmp[j][0];
+	const hi = cmp[j][1];
+	const msg = cmp[j][2];
+
+	for (let i in lines[lo].pts) {
+	    const pt = lines[lo].pts[i];
+	    if (pt[1] > lines[hi].get_y(pt[0])) {
+		msgs += msg;
+		return [false, changed, msgs];
+	    }
 	}
-	if (L_clone != null) {
-	    lines["L"] = L_clone;
-	    changed.push("L");
+	for (let i in lines[hi].pts) {
+	    const pt = lines[hi].pts[i];
+	    if (pt[1] < lines[lo].get_y(pt[0])) {
+		msgs += msg;
+		return [false, changed, msgs];
+	    }
+	}
+    }
+
+    // Condition on the bounds
+    const U = lines["U"].pts;
+    const A = lines["A"].pts;
+    for (let i in lines["U"].pts) {
+	if (i == 0)
+	    continue;
+	const slope = (U[i][1] - U[i-1][1]) / (U[i][0] - U[i-1][0]);
+	if (slope > C) {
+	    msgs += "The slope of the bounds must be <= C, because that is the rate at which tokens arrive. ";
+	    return [false, changed, msgs];
+	}
+	if (slope < C * (1 - 1e-6)) {
+	    // See if waste is allowed here
+	    if (U[i-1][1] < lines["A"].get_y(U[i-1][0]) ||
+	        U[i][1] < lines["A"].get_y(U[i][0])) {
+		msgs += "Waste (i.e. slope < C) can only happen when A(t) <= U(t). ";
+		return [false, changed, msgs];
+	    }
+	    for (let j in A) {
+		if (A[j][0] >= U[i-1][0] && A[j][0] <= U[i][0] &&
+		    A[j][1] > lines["U"].get_y(A[j][0])) {
+		    msgs += "Waste (i.e. slope < C) can only happen when A(t) <= U(t). ";
+		    return [false, changed, msgs];
+		}
+	    }
 	}
     }
 
@@ -329,21 +401,25 @@ function check_ccac_constraints(line_name) {
 }
 
 var D = 1;
+var C = 1;
 var buffer = 1;
-var T = 10;
+var T = 5;
+// Error margin
+const epsilon = 1e-6;
 
 // The set of lines
 var lines = {
-    "A": new Line("A", [[0, 0.1], [T, T]], "blue"),
-    "S": new Line("S", [[0, 0], [T, T-0.1]], "red"),
-    "U": new Line("U", [[0, 0], [T, T]], "black"),
-    "L": new Line("L", [[D, 0], [T+D, T]], "black"),
+    "A": new Line("A", [[0, 0.5*C], [T, C*T + 0.5*C]], "blue"),
+    "S": new Line("S", [[0, -0.5*C], [T, C*(T-0.1)]], "red"),
+    "U": new Line("U", [[0, 0], [T, C*T]], "black"),
+    "L": new Line("L", [[D, 0], [T+D, C*T]], "black"),
 };
 
-$(document).ready(function() {
-    let cum = new Graph("cumulative_graph", [0, 10], [0, 20]);
+var cum;
 
-    for (line in lines) {
+$(document).ready(function() {
+    cum = new Graph("cumulative_graph", [0, T + D], [0, 1.1 * C * T]);
+    for (let line in lines) {
 	cum.plot_line(lines[line]);
     }
 
